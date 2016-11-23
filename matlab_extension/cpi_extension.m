7401336affaff63f86573d243c58dedec2db0d43
rrhodes <ross.rhodes@hotmail.co.uk>
2016-11-23 14:44:07 +0000

    Amend script to submit CPi defs, not file path

--- matlab_extension/cpi_extension.m
+++ matlab_extension/cpi_extension.m
@@ -1,12 +1,45 @@
+% Load the CPiWB shared library
 if not(libisloaded('libOdeConstruction'))
     hsffi_path = '/usr/lib64/ghc-7.8.4/include';
     loadlibrary('libOdeConstruction', 'odeConstruction.h', 'includepath', hsffi_path);
 end
 
-filepath = 'test';
-process = 'Pi';
-result = calllib('libOdeConstruction', 'callCPiWB', strcat(filepath, ',', process));
-fprintf('%s\n', filepath);
-fprintf('%s\n', process);
-fprintf('%s\n', result);
+% Select an existing .cpi file
+[file_name, file_path, ~] = uigetfile;
+
+if (file_name == 0)
+    return;
+end
+
+cpi_defs = fileread(strcat(file_path, '/', file_name));
+disp(['\n', cpi_defs]);
+
+% allow user to choose a process to model
+process_found = [];
+
+while(isempty(process_found)) 
+    prompt = '\nPlease select a process from your chosen model.\nEnter ''quit'' to quit.\n> ';
+    process = input(prompt, 's');
+    
+    if (strcmp(process, '') == 1 || strcmp(process, 'quit') == 1 || strcmp(process, 'exit') == 1)
+        return;
+    end
+    
+    process_def = ['process ', process];
+    process_found = findstr(cpi_defs, process_def);
+    
+    if (isempty(process_found))
+        disp(['Error: Process ', process, ' not found. Please try again.']);
+    end
+end
+
+% temporary return statement until CPiWB side completed
+return;
+
+% call CPiWB to construct ODEs for the chosen process
+result = calllib('libOdeConstruction', 'callCPiWB', [cpi_defs, ',', process]);
+
+return;
+
+% free the library loaded at the start
 unloadlibrary('libOdeConstruction');
