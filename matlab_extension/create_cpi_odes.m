18f3b94a02cc73adf37b8ec7b11f2d7bbe6ca263
rrhodes <ross.rhodes@hotmail.co.uk>
2017-01-31 14:49:01 +0000

    Amend error to message string match in Matlab

--- matlab_extension/create_cpi_odes.m
+++ matlab_extension/create_cpi_odes.m
@@ -32,7 +32,7 @@ if (strcmp(cpiwb_result, 'parse error'))
     fprintf('\n\nThe CPi Workbench failed to parse your .cpi file. Please try again.');
     ode_num = 0;
     return;
-elseif (strcmp(cpiwb_result, 'process error'))
+elseif (strcmp(cpiwb_result, 'process not found'))
     fprintf(['\n\nThe CPi Workbench failed to to find process ', process, '. Please try again.']);
     ode_num = 0;
     return;
