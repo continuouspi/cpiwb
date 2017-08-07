14471c5d70c972e19bff77c2e4a6ae4c53654236
Chaonan Dai <michelleiloved.dai@hotmail.com>
2017-08-07 15:27:09 +0100

    Add files via upload

--- cpi_python/CPiSimulate/compareSolution.py
+++ cpi_python/CPiSimulate/compareSolution.py
@@ -14,30 +14,47 @@ class SingleCompareSolution():
         self.times_list = times_list
         self.separateplot_title = separateplot_title
 
-    def show_plot(self, select_one, plot_type):
+    def show_common_plot(self, select_one, plot_type):
+
+        if select_one != '':
+
+            tol_numlist = []
+            if self.same_list:
+                if select_one != 'all':  # if only some common species are required to be emphasized
+                    input_list = select_one.split()
+
+                    num_list = []
+                    for item in input_list:
+                        if item in self.same_list:
+                            num_list.append(item)
+                        else:
+                            print 'There is no common species named: ' + item +', it will be ignored here.'
+
+                    tol_numlist = num_list
+                elif select_one == 'all':
+                    tol_numlist = self.same_list
+
+            if plot_type == 'single':
+                singleplot_title = 'CPi Model: ' + ' and '.join(self.tol_title)
+                singleCompare.plotdiffs(singleplot_title, self.tol_label, self.input_comparelist, tol_numlist,
+                                        self.tol_solution, self.times_list)
+            elif plot_type == 'separate':
+                separateCompare.plotdiffs(self.separateplot_title, self.input_comparelist, tol_numlist,
+                                          self.tol_solution, self.times_list)
+            else:
+                print "Please input the correct plot type, 'single' or 'separate'."
+        else:
+            print "If you do not prefer to emphasise any species, please sue the 'show_plot' method."
+
+    def show_plot(self, plot_type):
 
         tol_numlist = []
-        if self.same_list:
-            if select_one != 'all' and select_one != '':  # if only some common species are required to be emphasized
-                input_list = select_one.split()
-
-                num_list = []
-                for item in input_list:
-                    if item in self.same_list:
-                        num_list.append(item)
-                    else:
-                        print 'There is no common species named: ' + item +', it will be ignored here.'
-
-                tol_numlist = num_list
-            elif select_one == 'all':
-                tol_numlist = self.same_list
-
         if plot_type == 'single':
             singleplot_title = 'CPi Model: ' + ' and '.join(self.tol_title)
             singleCompare.plotdiffs(singleplot_title, self.tol_label, self.input_comparelist, tol_numlist,
                                     self.tol_solution, self.times_list)
         elif plot_type == 'separate':
-            separateCompare.plotdiffs(self.separateplot_title, self.input_comparelist, tol_numlist,
-                                      self.tol_solution, self.times_list)
+            separateCompare.plotdiffs(self.separateplot_title, self.input_comparelist, tol_numlist, self.tol_solution,
+                                      self.times_list)
         else:
             print "Please input the correct plot type, 'single' or 'separate'."
